de4d7080940e8979ff09f3b199f76d4251fa82cb
admin <admin@ec2-16-170-173-104.eu-north-1.compute.amazonaws.com>
2024-10-11 20:52:23 +0000

    Added systemd dependency

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule ATradingSystem.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:mix_systemd, "~> 0.1"}
     ]
   end
 
