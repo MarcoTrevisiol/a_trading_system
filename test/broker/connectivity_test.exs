228b8b5270897ee46987788164edf53c58ad8aa5
voltamass <voltarelmassimo@gmail.com>
2024-10-28 19:38:32 +0100

    Removed Unused Tests from the Fake Server Example

--- test/broker/connectivity_test.exs
+++ test/broker/connectivity_test.exs
@@ -1,28 +1,12 @@
 defmodule Broker.ConnectivityTest do
   use ExUnit.Case, async: false
 
-  @success_repo_params %{name: "success-repo"}
-  @failure_repo_params %{name: "failed-repo"}
-
   setup do
     start_supervised(
       {Plug.Cowboy, scheme: :http, plug: BrokerDouble.FakeServer, options: [port: 8081]}
     )
 
     :ok
-  end
-
-  @tag :skip
-  test "create_repo when success" do
-    response = Broker.Connectivity.create_repo(@success_repo_params)
-
-    assert response == %{name: "success-repo", id: 1234}
-  end
-
-  @tag :skip
-  test "create_repo when failure" do
-    response = Broker.Connectivity.create_repo(@failure_repo_params)
-    assert response == %{error_message: "error message"}
   end
 
   # @tag :skip
