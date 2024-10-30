97249cc1f966cd6021fe479beb9aab46d85312c2
angiust <angelo.giustiniani@gmail.com>
2024-10-30 17:16:47 +0100

    added buy and hold tactis, change folders in lib/trading/

---
+++ lib/trading/tactic/buy_hold.ex
@@ -0,0 +1,18 @@
+defmodule BuyHold do
+  defp handle_event(
+         %Trading.Event.DayEnded{datetime: _datetime},
+         %{day_number: day_number},
+         _tactic_state
+       ) do
+      %Trading.Orders.Market{symbol: nil, quantity: 1}
+    end
+  end
+
+  def tactic do
+    %Trading.Tactic{
+      handle_event: fn e, ms, ts -> handle_event(e, ms, ts) end,
+      info: [:day_number],
+      risk: 1
+    }
+  end
+end
