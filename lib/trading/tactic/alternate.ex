075baa73ee587049e3327cc0cedc0e3165860347
admin <admin@ec2-16-170-173-104.eu-north-1.compute.amazonaws.com>
2025-03-13 22:25:46 +0000

    Fixed alternate tactic

--- lib/trading/tactic/alternate.ex
+++ lib/trading/tactic/alternate.ex
@@ -5,8 +5,8 @@ defmodule Trading.Tactic.Alternate do
 
   def handle_event(%Trading.Event.DayEnded{}, %{day_number: day_number}, _tactic_state) do
     case rem(day_number, 2) do
-      0 -> [%Trading.Orders.Market{symbol: nil, quantity: +2}]
-      1 -> [%Trading.Orders.Market{symbol: nil, quantity: -2}]
+      0 -> [%Trading.Orders.Market{symbol: nil, quantity: +1}]
+      1 -> [%Trading.Orders.Market{symbol: nil, quantity: -1}]
     end
   end
 
