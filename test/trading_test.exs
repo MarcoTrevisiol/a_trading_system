36c1de1ab7d919b326526baa980ad75279797ee4
admin <admin@ec2-16-170-173-104.eu-north-1.compute.amazonaws.com>
2025-03-13 21:13:20 +0000

    Fix trading test

--- test/trading_test.exs
+++ test/trading_test.exs
@@ -31,12 +31,7 @@ defmodule TradingTest do
     assert info == %{close: 57}
   end
 
-  # Funzione helper per confrontare due float
-  def approx_equal?(a, b, epsilon \\ 1.0e-4) do
-    abs(a - b) < epsilon
-  end
-
-  #@tag :skip
+  # @tag :skip
   test "work on real data" do
     prices_filename = "test/nq.csv"
 
@@ -50,8 +45,7 @@ defmodule TradingTest do
         data_source: prices_filename
       )
 
-    assert approx_equal?(net_return, -455.52)
-    #assert_in_delta(net_return, -455.52, 1.0e-4) #assert net_return == -455.52
+    assert_in_delta(net_return, -455.52, 1.0e-4)
   end
 
   test "raise error on invalid data source" do
